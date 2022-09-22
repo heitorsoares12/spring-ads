b907dd548ae9fffa8df6ea1442401dbd16c6c5b5
Heitor Soares <heitor.soaress@outlook.com>
2022-09-22 10:13:14 -0300

    Adicionando Client e seus metodos

---
+++ src/main/java/br/com/heitorsoares/IntroApp/model/ClientModel.java
@@ -0,0 +1,23 @@
+package br.com.heitorsoares.IntroApp.model;
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients")
+@Data
+public class ClientModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 1)
+    private String gender;
+
+    @Column(nullable = false, length = 50)
+    private String city;
+
+}
