16acf2899c75ae52e86e6b87ba6a9c3d5cad036c
Heitor Soares <heitor.soaress@outlook.com>
2022-09-27 10:11:41 -0300

    Inserindo PUT e DELETE no client

--- src/main/java/br/com/heitorsoares/IntroApp/controller/ClientController.java
+++ src/main/java/br/com/heitorsoares/IntroApp/controller/ClientController.java
@@ -3,6 +3,7 @@ package br.com.heitorsoares.IntroApp.controller;
 import br.com.heitorsoares.IntroApp.model.ClientModel;
 import br.com.heitorsoares.IntroApp.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,5 +29,21 @@ public class ClientController {
     @PostMapping
     public ClientModel save(@RequestBody ClientModel model){
         return service.save(model);
+    }
+
+    @PutMapping
+    public ClientModel update(@RequestBody ClientModel model) {
+        return service.update(model);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") long id){
+        var found = service.findById(id);
+        if(found.isPresent()){
+            service.delete(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return null;
+        }
     }
 }
